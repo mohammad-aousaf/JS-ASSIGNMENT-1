209ce8219d31b562ae179e296093e1d7ceddfa6e
mohammad-aousaf <126142085+mohammad-aousaf@users.noreply.github.com>
2023-03-29 11:55:33 +0500

    Add files via upload

---
+++ JS Assignment/question3.js
@@ -0,0 +1,10 @@
+// Declare a variable called “temperature” and assign it a value of 72. Convert the temperature to Celsius and store the 
+// result in a new variable called “celsius”. Display the value of celsius to the console
+
+// Formula to convert the temprature from Fahrenheit to Celcius:
+// C = (°F - 32) × 5 / 9
+
+let tempInFahrenheit = 72;
+let tempInCelcius = (tempInFahrenheit - 32) * (5 / 9);
+
+console.log(`Converted value in Celcius, ${tempInCelcius}`);
